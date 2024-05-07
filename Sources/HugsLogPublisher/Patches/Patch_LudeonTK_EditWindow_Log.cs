05b8c179b491758c7ae8833fd642385f7009227e
m00nl1ght <github@m00nl1ght.dev>
2024-05-07 16:40:57 +0200

    Refactor project configuration

--- Sources/HugsLogPublisher/Patches/Patch_LudeonTK_EditWindow_Log.cs
+++ Sources/HugsLogPublisher/Patches/Patch_LudeonTK_EditWindow_Log.cs
@@ -5,6 +5,10 @@ using LunarFramework.Patching;
 using UnityEngine;
 using Verse;
 
+#if RW_1_5_OR_GREATER
+using LudeonTK;
+#endif
+
 namespace HugsLogPublisher.Patches;
 
 [PatchGroup("Main")]
@@ -27,9 +31,15 @@ internal static class Patch_LudeonTK_EditWindow_Log
     private static void DoRevRowButton(ref float x, float y, string text, string tooltip, Action action)
     {
         var vector2 = Text.CalcSize(text);
-        var rect = new Rect(x - vector2.x - 16f, y, vector2.x + 16f, vector2.y + 2f);
-        if (Widgets.ButtonText(rect, text))
-            action();
+
+        #if RW_1_5_OR_GREATER
+            var rect = new Rect(x - vector2.x - 10f, y, vector2.x + 10f, 24f);
+            if (DevGUI.ButtonText(rect, text)) action();
+        #else
+            var rect = new Rect(x - vector2.x - 16f, y, vector2.x + 16f, vector2.y + 2f);
+            if (Widgets.ButtonText(rect, text)) action();
+        #endif
+
         if (!tooltip.NullOrEmpty())
             TooltipHandler.TipRegion(rect, (TipSignal) tooltip);
         x -= rect.width + 4f;
